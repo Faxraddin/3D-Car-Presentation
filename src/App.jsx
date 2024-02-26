d662ac85432f6b0fe7997a3c2c14bbb5c7dac159
Faxraddin <reydivonay@gmail.com>
2024-02-26 22:31:39 +0400

    setled a bit car ,made it closer

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import CanvasContainer from "./components/CanvasContainer"
 
 function App() {
   return (
-    <>
+    <main className="overflox-x-hidden">
+      <div className="h-screen w-full fixed top-0 z-10 hidden lg:block">
         <CanvasContainer/>
-    </>
+      </div>
+        
+    </main>
   )
 }
 

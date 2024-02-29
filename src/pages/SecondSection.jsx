f76ff0019857306eec264245bef699721152df83
Faxraddin <reydivonay@gmail.com>
2024-02-29 22:03:30 +0400

    sone 2 section

--- src/pages/SecondSection.jsx
+++ src/pages/SecondSection.jsx
@@ -2,7 +2,18 @@ import React from "react"
 
 const SecondSection = () => {
   return (
-    <div>SecondSection</div>
+    <div className="fontG w-full h-[820px] flex justify-center items-center mt-5">
+        <div className="w-[96%] h-full flex justify-between items-center">
+            <div className="text-[70px] verticalText">
+                <h1>Prue.</h1>
+                <h1>Rare.</h1>
+                <h1>Beautiful.</h1>
+            </div>
+            <div className="text-center">
+                <p className="text-[50px]"><span className="text-[70px] verticalText2 text-red-500">1989</span>Maihara</p>
+            </div>
+        </div>
+    </div>
   )
 }
 export default SecondSection
